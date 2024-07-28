a528508da784826c2dc77408fa8ae0db937af42e
Bethany Fannin <contact@bethanyfannin.com>
2024-07-28 16:22:20 +0000

    Refactor navList into its own navData component

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,10 @@ import NavList from './components/NavList.jsx';
 import Search from './components/Search.jsx';
 import BlogList from './components/Blog/BlogList.jsx';
 import blogData from './data/blogData.js';
+import navData from './data/navData.js';
 import './App.css';
 
 const App = () => {
-  const navList = [
-    {
-      title: 'Home',
-      url: '/',
-      objectID: 0,
-    },
-    {
-      title: 'About',
-      url: '/about',
-      objectID: 1,
-    },
-    {
-      title: 'Contact',
-      url: '/contact',
-      objectID: 2,
-    },
-    {
-      title: 'Services',
-      url: '/services',
-      objectID: 3,
-    },
-  ];
-
-  const [count, setCount] = React.useState(0);
-
-  const handleChange = (event) => {
-    setCount(event.target.value);
-    count + 1;
-  }
 
   const handleSearch = (event) => {
     console.log(event.target.value);
@@ -47,12 +19,11 @@ const App = () => {
 
   return (
     <>
-      <NavList list={navList}/>
+      <NavList list={navData}/>
       <h1>{welcome.title}</h1>
       <p>{welcome.message}</p>
       <Search onSearch={handleSearch}/>
       <BlogList data={blogData}/>
-      <button onClick={handleChange}>Click me!</button>
     </>
   );
 }
---
+++ src/data/navData.js
@@ -0,0 +1,24 @@
+const navData = [
+    {
+      title: 'Home',
+      url: '/',
+      objectID: 0,
+    },
+    {
+      title: 'About',
+      url: '/about',
+      objectID: 1,
+    },
+    {
+      title: 'Contact',
+      url: '/contact',
+      objectID: 2,
+    },
+    {
+      title: 'Services',
+      url: '/services',
+      objectID: 3,
+    },
+  ];
+
+export default navData;
